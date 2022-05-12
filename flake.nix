4b51650374dc10e6e63807239e2fdc52f52c0b5c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:12 -0300

    chore: re index eachdo

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."eachdo-main".dir   = "main";
+  inputs."eachdo-main".owner = "nim-nix-pkgs";
+  inputs."eachdo-main".ref   = "master";
+  inputs."eachdo-main".repo  = "eachdo";
+  inputs."eachdo-main".type  = "github";
+  inputs."eachdo-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."eachdo-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."eachdo-v0_1_0".dir   = "v0_1_0";
+  inputs."eachdo-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."eachdo-v0_1_0".ref   = "master";
+  inputs."eachdo-v0_1_0".repo  = "eachdo";
+  inputs."eachdo-v0_1_0".type  = "github";
+  inputs."eachdo-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."eachdo-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."eachdo-v0_1_1".dir   = "v0_1_1";
+  inputs."eachdo-v0_1_1".owner = "nim-nix-pkgs";
+  inputs."eachdo-v0_1_1".ref   = "master";
+  inputs."eachdo-v0_1_1".repo  = "eachdo";
+  inputs."eachdo-v0_1_1".type  = "github";
+  inputs."eachdo-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."eachdo-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
